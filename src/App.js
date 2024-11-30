f47da6e8026e9f3e964ecca66cd17bb47155448d
sspark89 <sosn3@naver.com>
2024-11-30 22:05:45 +0900

    5번 css 작성법 할 차례

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Hello from './component/Hello';
+import Wellcome from './component/Wellcome';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Buchet React</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  // const name = "Buchet";
+  // const naver = {
+  //   name : "Naver",
+  //   url : "https://www.naver.com"
+  // }
+
+  // return <div className="App">
+  //   <h1 style={{
+  //     color:"#fef",
+  //     backgroundColor:"green",
+  //   }}>Hello, {name}, <p>{ 2 + 3 }</p>
+  //   </h1>
+  //   <a href={naver.url}>{naver.name}</a>
+  // </div>
+  return <div className="App">
+    <Hello />
+    <Wellcome />
     </div>
-  );
 }
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>Buchet React</p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
---
+++ src/component/Wellcome.js
@@ -0,0 +1,3 @@
+export default function Wellcome() {
+    return <p>Wellcome</p>;
+}
